3ca8379911d1ec21063d2448078bb27d70ff6530
nitrotron <toddmetcalf@gmail.com>
2014-05-22 06:53:31 -0400

    initial commit

---
+++ SerialPortSwitchClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Collections;
+using SerialPortSwitchClient.cs.SerialSwitch;
+
+
+namespace SerialPortSwitchClient.cs
+{
+    class Program
+    {
+        public void printStatus(Dictionary<string, decimal> dict)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key.ToString() + " = " + item.Value.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            ArduinoSelfHostClient Client = new ArduinoSelfHostClient();
+            Program prog = new Program();
+
+            Dictionary<string, decimal> status = Client.GetStatus();
+
+            for (int i = 0; i < 15; i++)
+            {
+                Client.SendCommand(ArduinoCommandsCommandTypes.SetTempAlarmLow, "1,50");
+                Client.UpdateStatus();
+                System.Threading.Thread.Sleep(15000);
+                status = Client.GetStatus();
+                prog.printStatus(status);
+
+                Console.WriteLine("THermo 0 = " + status["Thermometer0"]);
+            }
+            Console.WriteLine("Hit Enter to exit");
+            Console.ReadLine();
+            
+        }
+    }
+}
